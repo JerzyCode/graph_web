dce743ad9e76c4dca8599821f4ca54b251993811
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-06-18 15:00:25 +0200

    feat: add bfs algorithm frontend

--- static/js/algorithm_service.js
+++ static/js/algorithm_service.js
@@ -1,4 +1,4 @@
-import {runDfsAlgorithmEndpoint} from "./endpoints.js";
+import {runBfsAlgorithmEndpoint, runDfsAlgorithmEndpoint} from "./endpoints.js";
 import {colorGraphAlgorithm} from "./canvas.js";
 import {currentLoadedGraph} from "./modify_graph_service.js";
 import {showFailMessage} from "./shared.js";
@@ -11,5 +11,16 @@ export async function runDfsAlgorithm() {
         await colorGraphAlgorithm(objectsToColor)
     } catch (error) {
         showFailMessage('Something went wrong running DFS!')
+    }
+}
+
+export async function runBfsAlgorithm() {
+    let graphId = currentLoadedGraph.graphId
+    console.log("Running DFS for graph with id=" + graphId)
+    try {
+        let objectsToColor = await runBfsAlgorithmEndpoint(graphId)
+        await colorGraphAlgorithm(objectsToColor)
+    } catch (error) {
+        showFailMessage('Something went wrong running BFS!')
     }
 }
--- static/js/graph_panel.js
+++ static/js/graph_panel.js
@@ -1,12 +1,15 @@
 import {repaint} from "./canvas.js";
 import {closeYourGraphsPopup, openYourGraphsPopup} from "./your_graphs_popup.js";
 import {addVertex, createGraph, deleteEdge, deleteVertex, selectToEdge} from "./modify_graph_service.js";
-import {runDfsAlgorithm} from "./algorithm_service.js";
+import {runBfsAlgorithm, runDfsAlgorithm} from "./algorithm_service.js";
 
 const addVertexPopup = document.getElementById('add-vertex-popup')
 const addVertexButton = document.getElementById('add-vertex-button')
 
 const runDfsButton = document.getElementById('dfs-algorithm-button')
+const runBfsButton = document.getElementById('bfs-algorithm-button')
+
+
 const selectEdgeButton = document.getElementById('select-to-edge-button')
 
 const deleteVertexButton = document.getElementById('delete-vertex-button')
@@ -43,6 +46,10 @@ const addListeners = function () {
     }
     if (runDfsButton) {
         runDfsButton.addEventListener('click', runDfsAlgorithm)
+    }
+
+    if (runBfsButton) {
+        runBfsButton.addEventListener('click', runBfsAlgorithm)
     }
 
     if (createGraphButton) {
